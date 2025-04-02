57495c600550b9935fff7af765207f21bc28a4f2
alcharra <168999056+alcharra@users.noreply.github.com>
2025-04-02 22:24:45 +0100

    feat: support environment variables via ENV_VARS input

--- deploy/files/verify.go
+++ deploy/files/verify.go
@@ -16,13 +16,13 @@ func CheckFilesExistRemote(client *ssh.Client, projectPath string, files []strin
 		remotePath := path.Join(projectPath, filename)
 
 		cmd := fmt.Sprintf(`
-		FILE_PATH="%s"
-		if [ ! -f "$FILE_PATH" ]; then
-			echo "❌ File missing after upload: $FILE_PATH"
-			exit 1
-		else
-			echo "✅ Verified file exists: $FILE_PATH"
-		fi
+			FILE_PATH="%s"
+			if [ ! -f "$FILE_PATH" ]; then
+				echo "❌ File missing after upload: $FILE_PATH"
+				exit 1
+			else
+				echo "✅ Verified file exists: $FILE_PATH"
+			fi
 		`, remotePath)
 
 		stdout, stderr, err := client.RunCommandBuffered(cmd)
